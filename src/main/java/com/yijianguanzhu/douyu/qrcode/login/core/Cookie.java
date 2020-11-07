d21e399662ef363449a1f63b015072f2a8e8bff5
yijianguanzhu <yijianguanzhu@gmail.com>
2020-11-07 16:28:28 +0800

    新增csrf token获取

--- src/main/java/com/yijianguanzhu/douyu/qrcode/login/core/Cookie.java
+++ src/main/java/com/yijianguanzhu/douyu/qrcode/login/core/Cookie.java
@@ -14,8 +14,10 @@ import org.apache.http.util.EntityUtils;
 import com.beust.jcommander.internal.Maps;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.yijianguanzhu.douyu.qrcode.login.core.config.DouyuHttpRequestConfig;
+import com.yijianguanzhu.douyu.qrcode.login.core.model.DouyuBaseResponseEntity;
 import com.yijianguanzhu.douyu.qrcode.login.enums.Mode;
 import com.yijianguanzhu.douyu.qrcode.login.exception.CookieRefreshFailedException;
+import com.yijianguanzhu.douyu.qrcode.login.exception.CsrfTokenFailedException;
 import com.yijianguanzhu.douyu.qrcode.login.http.Http;
 import com.yijianguanzhu.douyu.qrcode.login.utils.CookieUtil;
 import com.yijianguanzhu.douyu.qrcode.login.utils.Message2BeanUtil;
@@ -29,14 +31,14 @@ import lombok.extern.slf4j.Slf4j;
  *
  */
 @Slf4j
-public final class RefreshCookie {
+public final class Cookie {
 	private final static String COOKIE = "cookie";
 
 	public static Map<String, String> refresh( final Map<String, String> oldCookie ) {
 
 		log.info( "开始刷新cookie..." );
 		final Map<String, String> headers = Maps.<String, String> newLinkedHashMap();
-		headers.put( COOKIE, CookieUtil.map2CookieString( oldCookie ) );
+		headers.put( COOKIE, CookieUtil.cookies( oldCookie ) );
 		headers.putAll( DouyuHttpRequestConfig.REFRESH_COOKIE_URL_HEADERS );
 
 		final HttpResponse response = Http.getHttpResponse( DouyuHttpRequestConfig.REFRESH_COOKIE_URL, headers, Mode.GET );
@@ -69,6 +71,42 @@ public final class RefreshCookie {
 		log.info( "cookie刷新成功" );
 		final Map<String, String> newCookies = CookieUtil.cookies( response );
 		CookieUtil.fillCookies( oldCookie, newCookies );
+		return newCookies;
+	}
+
+	// 验证&&获取csrf
+	public static Map<String, String> csrfToken( final Map<String, String> cookie ) {
+
+		log.info( "验证&&获取csrf..." );
+		final Map<String, String> headers = Maps.<String, String> newLinkedHashMap();
+		headers.put( COOKIE, CookieUtil.cookies( cookie ) );
+		headers.putAll( DouyuHttpRequestConfig.CSRF_TOKEN_URL_HEADERS );
+
+		final HttpResponse response = Http.getHttpResponse( DouyuHttpRequestConfig.CSRF_TOKEN_URL, headers, Mode.GET );
+		DouyuBaseResponseEntity<String> data = null;
+
+		try {
+			String resp = EntityUtils.toString( response.getEntity(), StandardCharsets.UTF_8 );
+			data = Message2BeanUtil.bean( resp, new TypeReference<DouyuBaseResponseEntity<String>>() {
+			} );
+		}
+		catch ( ParseException | IOException e ) {
+			log.warn( "验证&&获取csrf失败." );
+			log.error( "Un-Excepted Error occured. Cause By: \n", e );
+			throw new CsrfTokenFailedException( e.getMessage() );
+		}
+		finally {
+			EntityUtils.consumeQuietly( response.getEntity() );
+		}
+
+		if ( data.getError() != 0 ) {
+			log.warn( "验证&&获取csrf失败，{}", data.toString() );
+			throw new CsrfTokenFailedException( data.getMsg() );
+		}
+
+		log.info( "验证&&获取csrf成功." );
+		final Map<String, String> newCookies = CookieUtil.cookies( response );
+		CookieUtil.fillCookies( cookie, newCookies );
 		return newCookies;
 	}
 }
